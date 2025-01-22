c2de98b08b6659577cf0adc0613090c7d0230844
blainefreestone <blainefreestone01@gmail.com>
2025-01-22 13:27:30 -0700

    mac module created for multiplying and accumulating for each value in a vector row

---
+++ mac.v
@@ -0,0 +1,18 @@
+module mac (
+    input reset, clk,
+    input [7:0] a, b, 
+    output reg [23:0] out
+);
+    reg [15:0] product_reg;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            product_reg <= 16'b0;
+            out <= 24'b0;
+        end else begin
+            product_reg  <= a * b;
+            out <= out + product_reg;
+        end
+    end
+
+endmodule
