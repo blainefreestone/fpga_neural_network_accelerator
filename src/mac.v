0e3b44530c39de75e22cdb10103019cbd51f33f1
blainefreestone <blainefreestone01@gmail.com>
2025-01-23 12:58:42 -0700

    parameterized the widths of inputs and outputs in mac

--- src/mac.v
+++ src/mac.v
@@ -1,15 +1,18 @@
-module mac (
+module mac #(
+    parameter WIDTH = 8,            // determines the width of the inputs
+    parameter ACCUMULATIONS = 3     // determines the number of accumulations will fit in the output (in the worst case)
+) (
     input reset, clk,
-    input wire [7:0] a, b, 
-    output reg [23:0] out
+    input wire [WIDTH - 1:0] a, b,
+    output reg [WIDTH + $clog2(ACCUMULATIONS) - 1:0] out    // the output width is the sum of the input widths plus extra bits for accumulations
 );
-    reg [15:0] product_reg;
+    reg [2 * WIDTH:0] product_reg;      // stores the product of a and b
 
     always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            product_reg <= 16'b0;
-            out <= 24'b0;
-        end else begin
+        if (reset) begin        // reset the output and the product register
+            product_reg <= 0;
+            out <= 0;
+        end else begin          // calculate the product and add it to the existing output
             product_reg  <= a * b;
             out <= out + product_reg;
         end
