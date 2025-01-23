2b2e64b65b6cbcb4d2cfb3ddb0e245fd5d6fd178
blainefreestone <blainefreestone01@gmail.com>
2025-01-23 07:57:15 -0700

    vector scaler multiplier module created with multiple macs in parallel

---
+++ src/vsm.v
@@ -0,0 +1,24 @@
+module vsm #(
+    SIZE = 6,
+) (
+    input wire clk,
+    input wire reset,
+    input wire [8 * SIZE-1:0] a,
+    input wire [7:0] b,
+    output wire [8 * SIZE-1:0] out
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < SIZE; i = i + 1) begin : mac_gen
+            mac mac_i (
+                .reset(reset),
+                .clk(clk),
+                .a(a[8 * i +: 8]),
+                .b(b),
+                .out(out[8 * i +: 8])
+            );
+        end
+    endgenerate
+    
+endmodule
