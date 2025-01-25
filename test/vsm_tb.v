ed85c91fc1df8b1c2064a27a2d2a9cd510c703ef
blainefreestone <blainefreestone01@gmail.com>
2025-01-25 06:18:17 -0700

    testing the enable and disable for vsm

--- test/vsm_tb.v
+++ test/vsm_tb.v
@@ -42,6 +42,7 @@ module test_vsm;
         // Wait for global reset
         #10;
         
+        // Test 1: 3 vectors, with enable
         // Test vector 1 (with reset)
         // vector = [0x01, 0x04, 0x07]
         // scalar = 0x01
@@ -53,6 +54,7 @@ module test_vsm;
         a = 24'h010407;
         b = 8'h01;
         #10;
+        
         // Test vector 2 (with accumulation)
         // vector = [0x02, 0x05, 0x08]
         // scalar = 0x02
@@ -68,18 +70,70 @@ module test_vsm;
         a = 24'h030609;
         b = 8'h03;
         #10;
-        $display("Test vector 1: output = %h, expected = 010407, %s", out, (out == 24'h010407) ? "PASS" : "FAIL");
+        $display("Test 1, Vector 1: output = %h, expected = 010407, %s", out, (out == 24'h010407) ? "PASS" : "FAIL");
         
         // wait for propogation delay
         a = 0;
         b = 0;
         #10
 
-        $display("Test vector 2: output = %h, expected = 050E17, %s", out, (out == 24'h050E17) ? "PASS" : "FAIL");
+        $display("Test 2, Vector 2: output = %h, expected = 050E17, %s", out, (out == 24'h050E17) ? "PASS" : "FAIL");
 
         #10
 
-        $display("Test vector 3: output = %h, expected = 0E2032, %s", out, (out == 24'h0E2032) ? "PASS" : "FAIL");
+        $display("Test 3, Vector 3: output = %h, expected = 0E2032, %s", out, (out == 24'h0E2032) ? "PASS" : "FAIL");
+        enable = 0;
+        
+        #10
+
+        // Test 2: 3 vectors, pause in between
+        // Test vector 1 (with reset)
+        // vector = [0x01, 0x04, 0x07]
+        // scalar = 0x01
+        // output should be [0x01, 0x04, 0x07]
+        reset = 1;
+        #10;
+        reset = 0;
+        enable = 1;
+        a = 24'h010407;
+        b = 8'h01;
+        #10;
+
+        enable = 0;
+        #10;
+        enable = 1;
+        
+        // Test vector 2 (with accumulation)
+        // vector = [0x02, 0x05, 0x08]
+        // scalar = 0x02
+        // output should be [0x05, 0x0E, 0x17]
+        a = 24'h020508;
+        b = 8'h02;
+        #10;
+
+        enable = 0;
+        #10;
+        enable = 1;
+
+        // Test vector 3 (with accumulation)
+        // vector = [0x03, 0x06, 0x09]
+        // scalar = 0x03
+        // output should be [0x0E, 0x20, 0x32]
+        a = 24'h030609;
+        b = 8'h03;
+        #10;
+        $display("Test 2, Vector 1: output = %h, expected = 010407, %s", out, (out == 24'h010407) ? "PASS" : "FAIL");
+        
+        // wait for propogation delay
+        a = 0;
+        b = 0;
+        #10
+
+        $display("Test 2, Vector 2: output = %h, expected = 050E17, %s", out, (out == 24'h050E17) ? "PASS" : "FAIL");
+
+        #10
+
+        $display("Test 3, Vector 3: output = %h, expected = 0E2032, %s", out, (out == 24'h0E2032) ? "PASS" : "FAIL");
         enable = 0;
         
         #10
