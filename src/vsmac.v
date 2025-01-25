cf4473db5e6edd43d1e34a77018c46b818e45050
blainefreestone <blainefreestone01@gmail.com>
2025-01-25 06:37:35 -0700

    commented

--- src/vsmac.v
+++ src/vsmac.v
@@ -1,8 +1,8 @@
 // vector scaler multiplier accumulator
 module vsmac #(
-    parameter SIZE = 6,
-    parameter WIDTH = 8,
-    parameter ACCUMULATIONS = 3
+    parameter SIZE = 6,             // determines the number of inputs (and in turn the number of MACs)
+    parameter WIDTH = 8,            // determines the width of the inputs
+    parameter ACCUMULATIONS = 3     // determines the number of accumulations it will perform before signalling done
 ) (
     input wire reset, clk, enable,
     input wire [8 * SIZE-1:0] a,
@@ -11,9 +11,10 @@ module vsmac #(
     output wire done
 );
 
-    wire [8 * SIZE-1:0] cur_out;
-    reg [$clog2(ACCUMULATIONS * 2) - 1:0] accumulation_counter;
+    wire [8 * SIZE-1:0] cur_out;    // wire to connect MAC outputs to output register
+    reg [$clog2(ACCUMULATIONS * 2) - 1:0] accumulation_counter;     // counter to keep track of the number of accumulations
 
+    // generate SIZE MACs
     genvar i;
     generate
         for (i = 0; i < SIZE; i = i + 1) begin : mac_gen
@@ -24,9 +25,9 @@ module vsmac #(
                 .reset(reset),
                 .clk(clk),
                 .enable(enable),
-                .a(a[8 * i +: 8]),
-                .b(b),
-                .out(cur_out[8 * i +: 8])
+                .a(a[8 * i +: 8]),          // slice a
+                .b(b),                      // b is common to all MACs
+                .out(cur_out[8 * i +: 8])   // slice out
             );
         end
     endgenerate
@@ -41,6 +42,6 @@ module vsmac #(
         end
     end
 
-    assign done = (accumulation_counter == ACCUMULATIONS * 2 - 1);
-    
+    assign done = (accumulation_counter == ACCUMULATIONS * 2 - 1);   // done when time for all accumulations ocurred
+
 endmodule
