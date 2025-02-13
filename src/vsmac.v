960c537ad3bec28d1dcf0797ff956f2a0c17a4b3
blainefreestone <blainefreestone01@gmail.com>
2025-02-13 15:49:30 -0700

    works with signed numbers and new tests for that

--- src/vsmac.v
+++ src/vsmac.v
@@ -5,13 +5,12 @@ module vsmac #(
     parameter ACCUMULATIONS = 3     // determines the number of accumulations it will perform before signalling done
 ) (
     input wire reset, clk, enable,
-    input wire [WIDTH * SIZE-1:0] a,
-    input wire [WIDTH - 1:0] b,
-    output reg [WIDTH * SIZE-1:0] out
+    input wire signed [WIDTH * SIZE-1:0] a,
+    input wire signed [WIDTH - 1:0] b,
+    output reg signed [WIDTH * SIZE-1:0] out
 );
 
-    wire [WIDTH * SIZE-1:0] cur_out;    // wire to connect MAC outputs to output register
-    reg [$clog2(ACCUMULATIONS * 2) - 1:0] accumulation_counter;     // counter to keep track of the number of accumulations
+    wire signed [WIDTH * SIZE-1:0] cur_out;    // wire to connect MAC outputs to output register
 
     // generate SIZE MACs
     genvar i;
@@ -34,13 +33,8 @@ module vsmac #(
     always @(negedge clk or posedge reset) begin
         if (reset) begin
             out <= 0;
-            accumulation_counter <= 0;
         end else if (enable) begin
-            accumulation_counter <= accumulation_counter + 1;
             out <= cur_out;
         end
     end
-
-    assign done = (accumulation_counter == ACCUMULATIONS * 2 - 1);   // done when time for all accumulations ocurred
-
 endmodule
